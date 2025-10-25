072469e14bdec24473a122d70af967fc53510f84
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-25 16:21:52 +0900

    refactor(get-winner-cars): 함수를 더 잘게 쪼개 리팩토링

--- src/get-winner-cars.js
+++ src/get-winner-cars.js
@@ -1,11 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 function getWinnerCars(cars) {
-  const MAX_POSITION = Math.max(...cars.map((car) => car.position));
-  const winners = cars
-    .filter((car) => car.position === MAX_POSITION)
-    .map((car) => car.name);
+  const winners = findWinnerNames(cars);
+  printWinners(winners);
+}
 
+function getMaxPosition(cars) {
+  return Math.max(...cars.map((car) => car.position));
+}
+
+function findWinnerNames(cars) {
+  const maxPosition = getMaxPosition(cars);
+  return cars
+    .filter((car) => car.position === maxPosition)
+    .map((car) => car.name);
+}
+
+function printWinners(winners) {
   const winnerNames = winners.join(", ");
   MissionUtils.Console.print(`최종 우승자 : ${winnerNames}`);
 }
