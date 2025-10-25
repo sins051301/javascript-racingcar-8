5f97554c22cb26a9f33cb06a75a2bde253df148d
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-25 14:05:13 +0900

    docs(readme): 초기 목표 세팅 및 초기 입력 받기 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = await MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)"
+      );
+      MissionUtils.Console.print(`결과 : ${carNames}`);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
