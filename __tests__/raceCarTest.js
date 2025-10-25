e2d721a7848056c562e92b00415d23d5117cdd94
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-25 16:15:17 +0900

    feat(race-car-test): 경주 함수에 대한 검증 추가

---
+++ __tests__/raceCarTest.js
@@ -0,0 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import raceCar from "../src/race-car.js";
+import Car from "../src/car.js";
+
+describe("raceCar", () => {
+  beforeEach(() => {
+    jest.spyOn(MissionUtils.Console, "print").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("3번 시도했는데 '-'가 4개 이상이면 오류", () => {
+    const car = new Car("pobi");
+    const cars = [car];
+    raceCar(cars, 3);
+
+    const trackLength = car.getTrack().length;
+
+    expect(trackLength).toBeLessThanOrEqual(3);
+  });
+});
