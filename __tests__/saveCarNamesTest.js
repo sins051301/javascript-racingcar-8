9c196997690d4591b174ea28bca1912639a9c84d
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-25 15:32:42 +0900

    feat(save-car-names): 동명이인 및 클래스 생성에 대한 검증 테스트 코드 생성

---
+++ __tests__/saveCarNamesTest.js
@@ -0,0 +1,30 @@
+import saveCarNames from "../src/save-car-names.js";
+import Car from "../src/car.js";
+
+describe("saveCarNames", () => {
+  test("Car 클래스가 정상적으로 생성되고 초기 position은 0이다", () => {
+    const input = ["carA", "carB"];
+    const cars = saveCarNames(input);
+
+    cars.forEach((car) => {
+      expect(car).toBeInstanceOf(Car);
+      expect(car.position).toBe(0);
+      expect(car.getTrack()).toBe("");
+    });
+  });
+
+  test("중복된 이름이 있을 경우 숫자를 붙여 고유 이름을 생성한다", () => {
+    const input = ["pobi", "woni", "pobi", "pobi", "woni"];
+
+    const cars = saveCarNames(input);
+
+    expect(cars).toHaveLength(5);
+    expect(cars.map((c) => c.name)).toEqual([
+      "pobi",
+      "woni",
+      "pobi2",
+      "pobi3",
+      "woni2",
+    ]);
+  });
+});
