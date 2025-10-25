ad3337619f3cdb7e5b6af4f71afb7fc4280ad1c0
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-25 14:26:04 +0900

    docs(readme): 자동차 class에 대한 목표 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import saveCarNames from "./save-car-names.js";
+import validateCarNames from "./validate-car-names.js";
 
 class App {
   async run() {
@@ -8,6 +9,7 @@ class App {
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)"
       );
       const carNames = saveCarNames(inputCarNames);
+      validateCarNames(carNames);
       MissionUtils.Console.print(`결과 : ${carNames}`);
     } catch (error) {
       MissionUtils.Console.print(error.message);
