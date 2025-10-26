bd7edaf089e5f15de398a7e2ff21b23373f0ee54
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-26 17:34:57 +0900

    refactor(save-car-names): 함수 분리를 통해 depth줄이기

--- src/input/save-car-names.js
+++ src/input/save-car-names.js
@@ -1,19 +1,25 @@
 import Car from "../car.js";
 
+function createUniqueName(name, nameCount) {
+  if (!nameCount[name]) {
+    nameCount[name] = 1;
+  } else {
+    nameCount[name]++;
+  }
+
+  let uniqueName = name;
+  if (nameCount[name] > 1) {
+    uniqueName = `${name}${nameCount[name]}`;
+  }
+
+  return uniqueName;
+}
+
 function saveCarNames(inputArray) {
   const nameCount = {};
 
   const cars = inputArray.map((name) => {
-    if (!nameCount[name]) {
-      nameCount[name] = 1;
-    } else {
-      nameCount[name]++;
-    }
-
-    let uniqueName = name;
-    if (nameCount[name] > 1) {
-      uniqueName = `${name}${nameCount[name]}`;
-    }
+    const uniqueName = createUniqueName(name, nameCount);
 
     return new Car(uniqueName);
   });
