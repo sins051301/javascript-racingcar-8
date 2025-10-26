de60e16f23ec841b1e3626676366b68c02cca366
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-26 17:50:52 +0900

    fix(App.js):출력 형식 변경

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App {
   async run() {
     try {
       const input = await MissionUtils.Console.readLineAsync(
-        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
       );
       const tryCount = await MissionUtils.Console.readLineAsync(
-        "시도할 횟수는 몇 회인가요?"
+        "시도할 횟수는 몇 회인가요?\n"
       );
       const inputArray = validateCarNames(input);
       const carNames = saveCarNames(inputArray);
