440fc8e31d1eb48baee16359a422dd9ed4487111
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-26 17:13:31 +0900

    fix(test): 입력 타이밍에 따른 테스트 케이스 오류 해결

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ class App {
       const input = await MissionUtils.Console.readLineAsync(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)"
       );
-      const inputArray = validateCarNames(input);
-      const carNames = saveCarNames(inputArray);
-
       const tryCount = await MissionUtils.Console.readLineAsync(
         "시도할 횟수는 몇 회인가요?"
       );
+      const inputArray = validateCarNames(input);
+      const carNames = saveCarNames(inputArray);
+
       const tryCountNumber = validateTryCount(tryCount);
       raceCar(carNames, tryCountNumber);
       getWinnerCars(carNames);
