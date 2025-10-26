230334a3d391e205bccfaed8defdf0e5676be3cb
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-26 18:11:31 +0900

    fix(validate-try-count): 검증 변수 수정

--- src/input/validate-try-count.js
+++ src/input/validate-try-count.js
@@ -1,14 +1,14 @@
 function validateTryCount(tryCount) {
-  const parsed = Number(tryCount);
+  const tryCountNumber = Number(tryCount);
 
-  if (isNaN(parsed)) {
+  if (isNaN(tryCountNumber)) {
     throw new Error("[ERROR] 시도 횟수는 정수여야 합니다.");
   }
 
-  if (parsed <= 0) {
+  if (tryCountNumber <= 0) {
     throw new Error("[ERROR] 시도 횟수는 0보다 커야 합니다.");
   }
-  return parsed;
+  return tryCountNumber;
 }
 
 export default validateTryCount;
