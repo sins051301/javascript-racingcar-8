00523ac86d39f5d037fba8e1c9d2ae4f3c496965
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-27 23:49:16 +0900

    fix(error): 에러처리리 수정

--- src/App.js
+++ src/App.js
@@ -7,23 +7,18 @@ import getWinnerCars from "./result/get-winner-cars.js";
 
 class App {
   async run() {
-    try {
-      const input = await Console.readLineAsync(
-        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-      );
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
 
-      const inputArray = validateCarNames(input);
-      const carNames = saveCarNames(inputArray);
-      const tryCount = await Console.readLineAsync(
-        "시도할 횟수는 몇 회인가요?\n"
-      );
-      const tryCountNumber = validateTryCount(tryCount);
-      raceCar(carNames, tryCountNumber);
-      getWinnerCars(carNames);
-    } catch (error) {
-      Console.print(error.message);
-      throw error;
-    }
+    const inputArray = validateCarNames(input);
+    const carNames = saveCarNames(inputArray);
+    const tryCount = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    const tryCountNumber = validateTryCount(tryCount);
+    raceCar(carNames, tryCountNumber);
+    getWinnerCars(carNames);
   }
 }
 
